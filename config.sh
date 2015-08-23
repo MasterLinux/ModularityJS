8b2e70d41edd65575c5cf4b5fc443a223aa6e478
Sven Lang <SvenLang07@gmail.com>
2015-08-23 17:32:51 +0200

    clean variable of $PWD

---
+++ config.sh
@@ -0,0 +1,4 @@
+
+
+SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env/ bash
 
-SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source ./config.sh
+
 cd $SCRIPT_PATH
 
 if [ ! -f $SCRIPT_PATH/node_modules/.bin/jake ]
