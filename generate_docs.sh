3dd0b2d66d45c9705734baf52dd770e8584df99c
Sven Lang <SvenLang07@gmail.com>
2015-09-11 23:27:14 +0200

    [fix] osx platform to generate the documentation

--- generate_docs.sh
+++ generate_docs.sh
@@ -2,4 +2,11 @@
 
 source ./config.sh
 
-./node_modules/.bin/jsdoc ./build/modularity.js -d ./build/docs/
+sh ./build.sh && echo "Start create documentation" &&
+
+if [ "$(uname)" == "Darwin" ]; then
+    # Do something under Mac OS X platform
+    ./node_modules/.bin/jsdoc ./build/modularity.js -d ./build/docs/ -c ./config_jsdoc.json
+else
+    ./node_modules/.bin/jsdoc ./build/modularity.js -d ./build/docs/
+fi
