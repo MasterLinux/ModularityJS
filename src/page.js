c24467f8c7a32072cb9cb5105d4e4fa583f90b4c
Christoph Grundmann <christoph.grundmann@gmail.com>
2015-10-03 00:11:49 +0200

    [added] static create function to create a page. [updated] navigateTo behaviour

--- src/page.js
+++ src/page.js
@@ -1,13 +1,38 @@
 import {Dictionary} from "./data/dictionary.js";
+import {EventResponder} from "./event_responder.js";
+import {MissingIdentifierError} from "./error/missing_identifier_error.js";
+import {NavigationError} from "./error/navigation_error.js";
+import * as TypeUtilities from "./utility/type_utility.js";
 
-export class Page {
+export class Page extends EventResponder {
 
-    constructor(parent = null, {id, title, pages = []} = {}) {
+    /**
+     *
+     * @param {(Page|EventResponder)} parent
+     * @param {String} id
+     * @param {String} [title]
+     * @param {Dictionary} [children]
+     */
+    constructor(parent, id, title = null, children = new Dictionary()) {
+        super(parent);
+
         this._id = id;
         this._title = title;
         this._nextPage = null;
         this._previousPage = parent;
-        this._navigationStack = new Dictionary(pages);
+        this._children = children;
+    }
+
+    static create(parent = null, {id, title, children = []} = {}) {
+        if (TypeUtilities.isString(id) && id.length > 0) {
+            try {
+                return new Page(parent, id, title, new Dictionary(children));
+            } catch (error) {
+                throw new MissingIdentifierError(`Unable to create child of page <${id}> because of an invalid or missing identifier. id must be a <String> and not empty`);
+            }
+        } else {
+            throw new MissingIdentifierError(`Unable to create page because of an invalid or missing identifier. id must be a <String> and not empty but is currently <${id}>`);
+        }
     }
 
     get id() {
@@ -19,7 +44,7 @@ export class Page {
     }
 
     get navigationStack() {
-        return this._navigationStack;
+        return this._children;
     }
 
     get nextPage() {
@@ -32,6 +57,13 @@ export class Page {
 
     navigateTo(pageId) {
         let pageConfig = this.navigationStack.getItem(pageId);
-        this._nextPage = new Page(this, pageConfig);
+
+        if (pageConfig) {
+            this._nextPage = Page.create(this, pageConfig);
+            return true;
+        } else {
+            this.propagateError(new NavigationError(this.id, pageId));
+            return false;
+        }
     }
 }
--- tests/page_tests.js
+++ tests/page_tests.js
@@ -1,5 +1,8 @@
 import {Page} from "../src/page.js";
 import {Dictionary} from "../src/data/dictionary.js";
+import {EventResponderMock} from "./mocks/event_responder_mock.js";
+import {MissingIdentifierError} from "../src/error/missing_identifier_error.js";
+import {NavigationError} from "../src/error/navigation_error.js";
 import {expect, assert} from "chai";
 
 (function () {
@@ -8,13 +11,40 @@ import {expect, assert} from "chai";
         it("should create page using config", (done) => {
             let expectedId = "test_id";
             let expectedTitle = "test_title";
-            let pageUnderTest = new Page(null, {
+            let pageUnderTest = Page.create(null, {
                 id: expectedId,
                 title: expectedTitle
             });
 
             expect(pageUnderTest.id).to.be.equal(expectedId);
             expect(pageUnderTest.title).to.be.equal(expectedTitle);
+
+            done()
+        });
+
+        it("should throw error on missing ID", (done) => {
+            // child ID is missing
+            expect(() => {
+                Page.create(null, {
+                    id: "test_id",
+                    title: "test_title",
+                    children: [{
+                        title: "test_title"
+                    }]
+                });
+            }).to.throw(MissingIdentifierError);
+
+            // page ID is missing
+            expect(() => {
+                Page.create(null, {});
+            }).to.throw(MissingIdentifierError);
+
+            // page ID is invalid
+            expect(() => {
+                Page.create(null, {
+                    id: ""
+                });
+            }).to.throw(MissingIdentifierError);
 
             done()
         });
@@ -30,10 +60,10 @@ import {expect, assert} from "chai";
                 title: expectedTitle
             };
 
-            let pageUnderTest = new Page(undefined, {
+            let pageUnderTest = Page.create(undefined, {
                 id: anotherExpectedId,
                 title: anotherExpectedTitle,
-                pages: [expectedPage]
+                children: [expectedPage]
             });
 
             expect(pageUnderTest.nextPage).to.be.null;
@@ -54,9 +84,39 @@ import {expect, assert} from "chai";
             expect(prevPageUnderTest.title).to.be.equal(anotherExpectedTitle);
 
             expect(prevPageUnderTest.previousPage).to.be.null;
-            expect(prevPageUnderTest.previousPage).to.be.null;
 
             done()
+        });
+
+        it("should return whether navigation was successful or not", (done) => {
+            let expectedId = "test_id";
+
+            let expectedPage = {
+                id: expectedId
+            };
+
+            let pageUnderTest = Page.create(undefined, {
+                id: "test_id_2",
+                children: [expectedPage]
+            });
+
+            expect(pageUnderTest.navigateTo(expectedId)).to.be.true;
+            expect(pageUnderTest.navigateTo("unavailable_page_id")).to.be.false;
+
+            done()
+        });
+
+        it("should propagate error on missing page on navigation", (done) => {
+            let responderUnderTest = new EventResponderMock(null, (actualError) => {
+                expect(actualError).to.be.an.instanceof(NavigationError);
+                done()
+            });
+
+            let pageUnderTest = Page.create(responderUnderTest, {
+                id: "test_id"
+            });
+
+            pageUnderTest.navigateTo("unavailable_page_id");
         });
 
     });
