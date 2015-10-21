1d63a23819486d34096bc6b0c4c29d1bfddd3d06
Christoph Grundmann <christoph.grundmann@gmail.com>
2015-10-21 23:22:49 +0200

    [added] NavigationBar

--- src/view/page_view.js
+++ src/view/page_view.js
@@ -5,7 +5,20 @@ export var PageView = React.createClass({
     render: function() {
         return (
             <div className="page">
+                <NavigationBar title={this.props.title} />
                 {this.props.children}
+            </div>
+        );
+    }
+
+});
+
+export var NavigationBar = React.createClass({
+
+    render: function() {
+        return (
+            <div className="navigation_bar">
+                {this.props.title}
             </div>
         );
     }
