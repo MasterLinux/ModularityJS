4dcdf782fe695c4f6dfd2c0d1a0084291ac06978
Christoph Grundmann <christoph.grundmann@gmail.com>
2015-11-10 18:42:16 +0100

    [updated] navigation bar and moved it into its own file. [updated] css style and [added] icons for menu and back navigation

--- src/view/page_view.js
+++ src/view/page_view.js
@@ -1,11 +1,12 @@
 import React from "react";
+import {NavigationBar} from "./navigation_bar.js";
 
 export var PageView = React.createClass({
 
     render: function () {
         return (
             <div className="page">
-                <NavigationBar title={this.props.title} previousTitle="Back">
+                <NavigationBar title={this.props.title} backTitle="Back" canNavigateBack={true}>
                     <NavigationBar.ActionButton title="test" />
                 </NavigationBar>
                 {this.props.children}
@@ -15,41 +16,5 @@ export var PageView = React.createClass({
 
 });
 
-export var NavigationBar = React.createClass({
 
-    render: function () {
-        return (
-            <div className="row navigation-bar">
-                <div className="column size-small-2 navigation-items left">
-                    <NavigationBar.BackButton title={this.props.previousTitle} />
-                </div>
-                <div className="column size-small-8 title">{this.props.title}</div>
-                <div className="column size-small-2 navigation-items right">
-                    {this.props.children}
-                </div>
-            </div>
-        );
-    }
-
-});
-
-NavigationBar.BackButton = React.createClass({
-
-    render: function () {
-        return (
-            <div className="back-button">{this.props.title}</div>
-        );
-    }
-
-});
-
-NavigationBar.ActionButton = React.createClass({
-
-    render: function () {
-        return (
-            <div className="action-button">{this.props.title}</div>
-        );
-    }
-
-});
 
