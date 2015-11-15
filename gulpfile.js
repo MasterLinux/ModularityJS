2840fe360eb772c040ccda48139dffae2bb4c58b
Sven Lang <SvenLang07@gmail.com>
2015-11-15 18:01:37 +0100

    [added] documentation

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,31 @@
 var gulp = require('gulp'),
     concat = require('gulp-concat'),
+    ESdoc = require("gulp-esdoc"),
+    JSdoc = require("gulp-jsdoc"),
     sass = require('gulp-sass');
 
 
+
+// This task can watch & solo run
 gulp.task('sass', function () {
-    // This task can watch & solo run
     gulp.src('./sass/**/*.scss')
         .pipe(sass.sync({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(concat('modularity.css'))
         .pipe(gulp.dest('./build'));
+});
+
+
+// Generate two documentations in differents folder.
+gulp.task('generate documentation', function () {
+
+    // JSDoc
+    gulp.src("./build/modularity.js")
+        .pipe(JSdoc('./build/docs/jsdoc'));
+
+    // ESDoc
+    gulp.src("./src")
+        .pipe(ESdoc({
+            destination: "./build/docs/esdoc",
+            includeSource: true
+        }));
 });
