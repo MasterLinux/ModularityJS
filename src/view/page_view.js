4c9772b52072b05048dfb3496a84428002bb75f5
Christoph Grundmann <christoph.grundmann@gmail.com>
2016-06-02 21:25:10 +0200

    [added] Workspace and Line element

--- src/view/page_view.js
+++ src/view/page_view.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {NavigationBar} from "./navigation_bar.js";
 import {Button} from "./button.js";
+import {Workspace, Line, Coordinate} from "./workspace";
 
 export var PageView = React.createClass({
 
@@ -8,9 +9,12 @@ export var PageView = React.createClass({
         return (
             <div className="page">
                 <NavigationBar title={this.props.title} backTitle="Back" canNavigateBack={true}>
-                    <Button />
+                    <Button title="Bla" />
                 </NavigationBar>
                 {this.props.children}
+                <Workspace>
+                    <Line coordinates={[new Coordinate(0, 0), new Coordinate(25, 25), new Coordinate(100, 75)]} />
+                </Workspace>
             </div>
         );
     }
---
+++ src/view/workspace.js
@@ -0,0 +1,65 @@
+/**
+ * Created by Christoph on 01/06/2016.
+ */
+
+import React from "react";
+
+export class Coordinate {
+
+    constructor(x, y) {
+        this._x = x;
+        this._y = y;
+    }
+
+    get x() {
+        return this._x;
+    }
+
+    get y() {
+        return this._y;
+    }
+
+}
+
+export var Workspace = React.createClass({
+
+    propTypes: {
+        width: React.PropTypes.number,
+        height: React.PropTypes.number,
+        children: React.PropTypes.oneOfType([
+            React.PropTypes.instanceOf(Line)
+        ])
+    },
+
+    getDefaultProps: function() {
+        return {
+            width: 100,
+            height: 100
+        };
+    },
+
+    render: function () {
+        return <svg className="workspace" height={`${this.props.height}px`} width={`${this.props.width}px`}>{this.props.children}</svg>;
+    }
+
+});
+
+export var Line = React.createClass({
+
+    propTypes: {
+        coordinates: React.PropTypes.arrayOf(Coordinate)
+    },
+
+    // onClick: function (event) {
+    //     alert(event.clientX);
+    // },
+
+    render: function () {
+        let points = this.props.coordinates.map((coordinate) => {
+              return `${coordinate.x},${coordinate.y}`;
+        }).join(" ");
+
+        return <polyline className="line" points={points} />;
+    }
+
+});
